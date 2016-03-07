ea88f51e789760660d37913cdb62efca492a7e16
Patrick Gruber <patrick@gruber.fyi>
2016-03-07 14:46:49 -0600

    changes

--- modules/gate/lib/gate.js
+++ modules/gate/lib/gate.js
@@ -1,11 +1,24 @@
 module.exports = {
-  
-  init: function(){
-    
+
+  aClients: [],
+
+  init: function(aClients){
+    // detect keypad input with js event
+    // => if right: open gate by tiggering gate open command
+    // check for gate open command
+    // => if received: open gate and log client
+    this.aClients = aClients;
   },
-  
+
   evaluateCommand: function(sCommand, aParams){
+    switch(sCommand){
+      case "openGate":
+        this.openGate(aParams[0]);
+    }
+  },
+
+  openGate: function(sClient){
     
   }
-  
+
 };
